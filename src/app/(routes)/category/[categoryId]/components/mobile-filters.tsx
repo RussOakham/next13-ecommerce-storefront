a34aa2e7c4ac78844010a6fd9de3dfb8e3f38d0e
RussOakham <73036915+RussOakham@users.noreply.github.com>
2023-07-16 13:15:51 +0100

    category page finished - mobile and desktop filters added

--- src/app/(routes)/category/[categoryId]/components/mobile-filters.tsx
+++ src/app/(routes)/category/[categoryId]/components/mobile-filters.tsx
@@ -1,15 +1,55 @@
 'use client'
 
+import { useState } from 'react'
+import { Dialog } from '@headlessui/react'
+import { Plus, X } from 'lucide-react'
+
+import Button from '@/components/ui/button'
+import IconButton from '@/components/ui/icon-button'
 import { Color, Size } from '@/types'
+
+import Filter from './filter'
 
 interface MobileFiltersProps {
 	colors: Color[]
 	sizes: Size[]
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const MobileFilters = ({ colors, sizes }: MobileFiltersProps) => {
-	return <div>MobileFilters</div>
+	const [open, setOpen] = useState(false)
+
+	const onOpen = () => setOpen(true)
+	const onClose = () => setOpen(false)
+
+	return (
+		<>
+			<Button className="flex items-center gap-x-2 lg:hidden" onClick={onOpen}>
+				Filters <Plus size={20} className="ml-2" />
+			</Button>
+
+			<Dialog
+				open={open}
+				onClose={onClose}
+				as="div"
+				className="relative z-40 lg:hidden"
+			>
+				{/* Background */}
+				<div className="fixed inset-0 bg-black bg-opacity-25" />
+				{/* Dialog position */}
+				<div className="fixed inset-0 z-40 flex">
+					<Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-6 shadow-xl">
+						<div className="flex items-center justify-end px-4">
+							<IconButton icon={<X size={15} />} onClick={onClose} />
+						</div>
+						<div className="p-4">
+							<Filter valueKey="sizeId" name="Sizes" data={sizes} />
+							<Filter valueKey="colorId" name="Colors" data={colors} />
+						</div>
+					</Dialog.Panel>
+				</div>
+			</Dialog>
+		</>
+	)
 }
 
 export default MobileFilters
