63a608e46f8b04441e3c6dbcee59898959d91886
RussOakham <73036915+RussOakham@users.noreply.github.com>
2023-07-16 21:48:13 +0100

    fixed axios instance so works correctly in production

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 let baseUrl = 'http://localhost:3030'
 
 if (process.env.NODE_ENV !== 'development') {
-	baseUrl = 'https://next13-ecommerce-admin-obqpssrxz-russoakham.vercel.app/'
+	baseUrl = 'https://next13-ecommerce-storefront.vercel.app/'
 }
 
 export default axios.create({
